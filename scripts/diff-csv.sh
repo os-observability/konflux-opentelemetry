1f76c9037cff8f7bc93b6453f6c82f9539f5c61f
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-01-21 16:12:38 +0100

    generalize diff-csv script
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- scripts/diff-csv.sh
+++ scripts/diff-csv.sh
@@ -7,21 +7,22 @@ set -eu
 # This is helpful if upstream changed the ClusterServiceVersion in a way which requires downstream changes to the patch script.
 #
 
-DIR="${PWD##*/}"
+PRODUCT="${PWD##*/konflux-}"
 
-if [[ "$DIR" = "konflux-jaeger" ]]; then
-  podman build -t jaeger-bundle -f Dockerfile.bundle . && podman cp $(podman create jaeger-bundle):/manifests/jaeger-operator.clusterserviceversion.yaml .
+podman build -t operator-bundle -f Dockerfile.bundle .
+podman create --name operator-bundle operator-bundle
+podman cp operator-bundle:/manifests/${PRODUCT}-operator.clusterserviceversion.yaml .
+
+if [[ "$PRODUCT" = "jaeger" ]]; then
   git diff --no-index jaeger-operator/bundle/manifests/jaeger-operator.clusterserviceversion.yaml jaeger-operator.clusterserviceversion.yaml || true
-  rm jaeger-operator.clusterserviceversion.yaml
-elif [[ "$DIR" = "konflux-opentelemetry" ]]; then
-  podman build -t opentelemetry-bundle -f Dockerfile.bundle . && podman cp $(podman create opentelemetry-bundle):/manifests/opentelemetry-operator.clusterserviceversion.yaml .
+elif [[ "$PRODUCT" = "opentelemetry" ]]; then
   git diff --no-index opentelemetry-operator/bundle/openshift/manifests/opentelemetry-operator.clusterserviceversion.yaml opentelemetry-operator.clusterserviceversion.yaml || true
-  rm opentelemetry-operator.clusterserviceversion.yaml
-elif [[ "$DIR" = "konflux-tempo" ]]; then
-  podman build -t tempo-bundle -f Dockerfile.bundle . && podman cp $(podman create tempo-bundle):/manifests/tempo-operator.clusterserviceversion.yaml .
+elif [[ "$PRODUCT" = "tempo" ]]; then
   git diff --no-index tempo-operator/bundle/openshift/manifests/tempo-operator.clusterserviceversion.yaml tempo-operator.clusterserviceversion.yaml || true
-  rm tempo-operator.clusterserviceversion.yaml
 else
   echo "This script must be run from a Konflux data repository."
   exit 1
 fi
+
+podman rm operator-bundle > /dev/null
+rm *.clusterserviceversion.yaml
