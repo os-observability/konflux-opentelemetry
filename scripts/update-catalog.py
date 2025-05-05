d515d66d172f9882589005097c9bff83ba183ee9
Pavol Loffay <p.loffay@gmail.com>
2025-05-05 19:46:53 +0200

    reuse script
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- scripts/update-catalog.py
+++ scripts/update-catalog.py
@@ -84,30 +84,15 @@ def update_catalog_template(bundle_pullspec):
 def update_catalog():
     if application == "jaeger":
         subprocess.run("""
-        opm alpha render-template basic --output yaml catalog/catalog-template.yaml > catalog/jaeger-product/catalog.yaml &&
-        opm alpha render-template basic --output yaml --migrate-level bundle-object-to-csv-metadata catalog/catalog-template.yaml > catalog/jaeger-product-4.17/catalog.yaml &&
-        sed -i 's#quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-bundle#registry.redhat.io/rhosdt/jaeger-operator-bundle#g' catalog/jaeger-product/catalog.yaml &&
-        sed -i 's#quay.io/redhat-user-workloads/rhosdt-tenant/jaeger/jaeger-bundle#registry.redhat.io/rhosdt/jaeger-operator-bundle#g' catalog/jaeger-product-4.17/catalog.yaml &&
-        opm validate catalog/jaeger-product &&
-        opm validate catalog/jaeger-product-4.17
+        ./scripts/update-catalog.sh jaeger
         """, shell=True, check=True)
     elif application == "otel":
         subprocess.run("""
-        opm alpha render-template basic --output yaml catalog/catalog-template.yaml > catalog/opentelemetry-product/catalog.yaml &&
-        opm alpha render-template basic --output yaml --migrate-level bundle-object-to-csv-metadata catalog/catalog-template.yaml > catalog/opentelemetry-product-4.17/catalog.yaml &&
-        sed -i 's#quay.io/redhat-user-workloads/rhosdt-tenant/otel/opentelemetry-bundle#registry.redhat.io/rhosdt/opentelemetry-operator-bundle#g' catalog/opentelemetry-product/catalog.yaml &&
-        sed -i 's#quay.io/redhat-user-workloads/rhosdt-tenant/otel/opentelemetry-bundle#registry.redhat.io/rhosdt/opentelemetry-operator-bundle#g' catalog/opentelemetry-product-4.17/catalog.yaml &&
-        opm validate catalog/opentelemetry-product &&
-        opm validate catalog/opentelemetry-product-4.17
+        ./scripts/update-catalog.sh opentelemetry
         """, shell=True, check=True)
     if application == "tempo":
         subprocess.run("""
-        opm alpha render-template basic --output yaml catalog/catalog-template.yaml > catalog/tempo-product/catalog.yaml &&
-        opm alpha render-template basic --output yaml --migrate-level bundle-object-to-csv-metadata catalog/catalog-template.yaml > catalog/tempo-product-4.17/catalog.yaml &&
-        sed -i 's#quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-bundle#registry.redhat.io/rhosdt/tempo-operator-bundle#g' catalog/tempo-product/catalog.yaml &&
-        sed -i 's#quay.io/redhat-user-workloads/rhosdt-tenant/tempo/tempo-bundle#registry.redhat.io/rhosdt/tempo-operator-bundle#g' catalog/tempo-product-4.17/catalog.yaml &&
-        opm validate catalog/tempo-product &&
-        opm validate catalog/tempo-product-4.17
+        ./scripts/update-catalog.sh tempo
         """, shell=True, check=True)
 
 def main():
