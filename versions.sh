8268df9aa70cf2668db526f65212840571d5aa59
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-09-30 16:31:04 +0200

    Add versions.sh
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

---
+++ versions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eu
+
+# Propagates version information to various static files.
+# This file is intentionally kept as a shellscript, to simplify product-specific modifications.
+
+echo "Fetching tags of all submodules..."
+git submodule foreach --recursive "git fetch --tags" > /dev/null 2>&1
+
+OPERATOR_VERSION=$(cd opentelemetry-operator && awk -F= '/^opentelemetry-collector=/ {print $2}' versions.txt)
+COLLECTOR_VERSION=$(cd redhat-opentelemetry-collector && yq '.dist.version' manifest.yaml)
+
+RHOSDT_VERSION=3.7
+BUNDLE_VERSION=${OPERATOR_VERSION}-1
+PREVIOUS_BUNDLE_VERSION=0.127.0-2
+MIN_OPENSHIFT_VERSION=4.12
+
+echo "Updating version numbers in Dockerfiles and bundle..."
+echo
+echo "Operator: ${OPERATOR_VERSION}"
+echo "Collector: ${COLLECTOR_VERSION}"
+echo "Bundle: ${BUNDLE_VERSION} (previous: ${PREVIOUS_BUNDLE_VERSION})"
+echo "Min OpenShift version: ${MIN_OPENSHIFT_VERSION}"
+
+# container labels
+sed -Ei "s/ARG VERSION=.*/ARG VERSION=${BUNDLE_VERSION}/g" Dockerfile.*
+sed -Ei "s/cpe=[^ ]*/cpe=\"cpe:\/a:redhat:openshift_distributed_tracing:${RHOSDT_VERSION}::el8\"/g" Dockerfile.*
+sed -Ei "s/com.redhat.openshift.versions=[^ ]*/com.redhat.openshift.versions=v${MIN_OPENSHIFT_VERSION}/g" Dockerfile.bundle
+
+# CSV
+sed -Ei "s/  version: .*/  version: ${BUNDLE_VERSION}/g" bundle-patch/patch_csv.yaml
+sed -Ei "s/name: opentelemetry-operator.v.*/name: opentelemetry-operator.v${BUNDLE_VERSION}/g" bundle-patch/patch_csv.yaml
+sed -Ei "s/replaces: opentelemetry-operator.v.*/replaces: opentelemetry-operator.v${PREVIOUS_BUNDLE_VERSION}/g" bundle-patch/patch_csv.yaml
+sed -Ei "s/olm.skipRange: '>=(.*) <[^']*/olm.skipRange: '>=\1 <${BUNDLE_VERSION}/g" bundle-patch/patch_csv.yaml
